e9b9b467d27883e14ba7680850ea13b8a0e5aadd
QZH2022 <97078565+QZH2022@users.noreply.github.com>
2022-01-05 13:32:58 +0800

    Update hap_stat.sh

--- hap_stat.sh
+++ hap_stat.sh
@@ -5,6 +5,12 @@ do
 	 a=($line) 
 	 r1=${a[0]} 
 	 r2=${a[1]} 
+	 
+	 # There are two choices for the "bed" option.
+	 # 1. Use corresponding high-confident bed files when comparing query vcf with golden standards (GIAB and GSCG). 
+	 # 2. Use "" when comparing query vcf with GATK results.
+	 bed=${a[2]} 
+	 
 	 fastqName=`basename $r2` 
 	 prefix=${fastqName%%.*} 
 	 cur_path=$(pwd)
@@ -12,6 +18,6 @@ do
 	 if [[ ! -d $path ]];then
 		mkdir -p $path 
 	 fi 
-	docker run --rm -v /NAS/sl:/NAS/sl pkrusche/hap.py /opt/hap.py/bin/hap.py $cur_path/$r1 $cur_path/$r2 -r /NAS/sl/hg38/Homo_sapiens_assembly38.fasta -T $cur_path/HG002_GRCh38_GIAB.bed -o $path/$prefix --engine vcfeval 
+	docker run --rm -v /NAS/sl:/NAS/sl pkrusche/hap.py /opt/hap.py/bin/hap.py $cur_path/$r1 $cur_path/$r2 -r /NAS/sl/hg38/Homo_sapiens_assembly38.fasta -T $bed -o $path/$prefix --engine vcfeval 
 done
 
